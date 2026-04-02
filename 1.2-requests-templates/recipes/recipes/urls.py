d11ddd34b2665e9f6213f9b4cc5c475e21025ef7
Алексей <cunxpofazatron@gmail.com>
2026-04-02 07:52:22 +0300

    done 1.2 recipes

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.contrib import admin
 from django.urls import path
+from calculator.views import recipes_view
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('admin/', admin.site.urls),
+    path('recipes/', recipes_view),
 ]

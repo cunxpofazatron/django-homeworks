6751b87f2d8789c4ff243acbfc578c13beef79ee
Алексей <cunxpofazatron@gmail.com>
2026-04-04 11:30:32 +0300

    final orm migrations

---
+++ 2.2-databases-2/orm_migrations/query.py
@@ -0,0 +1,23 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
+django.setup()
+
+from school.models import Teacher, Student
+
+
+def get_students_by_teacher(teacher_name):
+    students = Student.objects.filter(teacher__name__icontains=teacher_name)
+
+    if not students:
+        print("Ничего не найдено")
+        return
+
+    for s in students:
+        print(f"{s.name} ({s.group}) — {s.teacher.name}")
+
+
+if __name__ == "__main__":
+    name = input("Введите имя учителя: ")
+    get_students_by_teacher(name)

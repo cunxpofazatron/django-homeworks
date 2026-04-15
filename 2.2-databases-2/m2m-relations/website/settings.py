8fa17987654fef28577450430f76d9b66c6582b2
cunxpofazatron <cunxpofazatron@gmail.com>
2026-04-15 14:18:39 +0300

    update

--- 2.2-databases-2/m2m-relations/website/settings.py
+++ 2.2-databases-2/m2m-relations/website/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'b0e@^m&tccz11$w59qov$lhn-97!(%wfn-gray-c*x)^a$wx=2'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_db',        
+        'NAME': 'django_db',
         'USER': 'django_user',
         'PASSWORD': '1234',
         'HOST': '127.0.0.1',
